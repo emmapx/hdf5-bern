dc70139755a58cbaed602b168121a4ce90303867
emmapx <72860990+emmapx@users.noreply.github.com>
2024-02-16 13:54:59 +0100

    hdf5.py
    The scaling is fixed and the only thing you have to change in order to run it is setting the dir and filename.

--- hdf5_test.py
+++ hdf5_test.py
@@ -3,19 +3,21 @@ import mne
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from mne.preprocessing import ICA
+import matplotlib.pyplot as plt
 
+# Change the directory and filename
 # Set the working directory to the location of your .hdf5 files
 os.chdir("C:\\Users\\peters\\OneDrive - Universitaet Bern\\Dokumente\\hdf5")
-
-cwd = os.getcwd()
-
+# Define the filename
+filename = 'BE01AN - 3_REM 1'
+#___________________________________________________________________________________#
 # Open the .hdf5 file
-with h5py.File('BA12ED - 3_REM 1.hdf5', 'r') as f:
+with h5py.File(f'{filename}.hdf5', 'r') as f:
     # Print the names of all datasets in the file
     for name in f.keys():
         print(name)
     # Extract the data and metadata
-        
     channels = ['01_F3', '02_F4', '03_C3', '04_C4', '05_O2', '06_O1', '07_EOGH1', '08_EOGH2', '09_EMG', '10_EMG']
     eeg_data = {channel: f[channel][:] for channel in channels}
     sampling_rate = 265
@@ -24,8 +26,10 @@ with h5py.File('BA12ED - 3_REM 1.hdf5', 'r') as f:
 data_2d = np.vstack(list(data.values()))
 info = mne.create_info(ch_names=list(data.keys()), sfreq=sampling_rate)
 raw = mne.io.RawArray(data_2d, info)
-raw.save('BA12ED - 3_REM 1_eeg.fif', overwrite=True)
-raw = mne.io.read_raw_fif('BA12ED - 3_REM 1_eeg.fif', preload=True)
+raw.save(f'{filename}_eeg.fif', overwrite=True)
+raw = mne.io.read_raw_fif(f'{filename}_eeg.fif', preload=True)
+cwd = os.getcwd()
+
 
 # Creating epochs
 epoch_duration = 30.  # duration of each epoch in secondsh
@@ -52,13 +56,41 @@ info = mne.create_info(channel_names, sampling_rate, channel_types)
 # Create an epochs object from the epochs data
 epochs = mne.EpochsArray(epochs_data, info)
 
+#for i in range(len(epochs)):
+    # Apply a Hann window function to each epoch
+    #epochs._data[i] *= np.hanning(epochs._data.shape[2])
+
 # Define the scaling factors for each channel type
-scalings = {'eeg': 1e-6, 'eog': 1e-6,}
-# Setting the channel names
+scalings = {'eeg': 0.5e2}
+
+# Setting the channel names 
 picks = ['01_F3', '02_F4', '03_C3', '04_C4', '05_O2', '06_O1', '07_EOGH1', '08_EOGH2', '09_EMG', '10_EMG']
 channel_map = {old_name: new_name for old_name, new_name in zip(epochs.ch_names, picks)}
 epochs.rename_channels(channel_map)
 
-# Plot the epochs with the specified scaling
-epochs.plot(n_epochs=1, n_channels=10, scalings=scalings)
+# Setting the channel types
+    # Get the names of 'misc' channels
+misc_channels = [raw.ch_names[i] for i in mne.pick_types(raw.info, misc=True)]
+    # Define a dictionary with new channel types
+new_channel_types = {name: 'eeg' for name in misc_channels}
+    # Change the channel types
+raw.set_channel_types(new_channel_types)
+
+# Calculate the signal length in samples
+signal_length_samples = len(epochs.times)
+
+# Set the filter length to be less than the signal length
+filter_length = int(signal_length_samples * 0.9)  # 90% of the signal length
+
+epochs.filter(l_freq=1, h_freq=30, filter_length=filter_length)
+epochs.plot(n_epochs=1, n_channels=10, scalings=scalings, title='Epochs')
+plt.show(block=True)
+
+# Create a figure and axes
+fig, ax = plt.subplots()
+
+# Plot the first channel of the first epoch
+ax.plot(epochs.times, data[0, 0])
+
+# Show the plot
 plt.show(block=True)
